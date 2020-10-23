f44c1220749d1e8adc9c139f75923a8edd08592d
Diego Chagas <drochachagas@gmail.com>
2020-10-23 15:28:55 -0300

    Updates

---
+++ precache-manifest.adc04b75399fb2f12504ff7b2bf8a6e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876f2539d6013b24546e6eb5cdb6b006",
+    "url": "/test-github-api/index.html"
+  },
+  {
+    "revision": "ccd7f7f6fe75e1e0550c",
+    "url": "/test-github-api/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "ccd7f7f6fe75e1e0550c",
+    "url": "/test-github-api/static/js/2.5a94ac08.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/test-github-api/static/js/2.5a94ac08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499093191d7b45fd6e48",
+    "url": "/test-github-api/static/js/main.564bb2bf.chunk.js"
+  },
+  {
+    "revision": "bbf87f2efde2f68aad90",
+    "url": "/test-github-api/static/js/runtime-main.acfb5ef8.js"
+  }
+]);
